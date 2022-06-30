58772011939276052f68683f145f0b5cfdcc52d1
AVacAdok <avakuang8@gmail.com>
2022-06-30 05:15:13 +0000

    sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-//const id = process.argv[3];
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const queryString =`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+//$1 id the placeholder for the actual data from the values array below --> values [...]
+const cohortName = process.argv[2];
+const values = [`${cohortName}`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
